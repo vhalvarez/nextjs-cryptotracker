ebb8326d4bffa495ec841b3ef0cf0f49117e7cca
vhalvarez <alvarez.programmer@gmail.com>
2021-05-03 10:29:21 -0400

    Crypto Tracker Finish

---
+++ components/SearchBar/index.js
@@ -0,0 +1,14 @@
+import styles from './Search.module.css'
+
+const SearchBar = ({...rest}) => {
+    return (
+        <div className={styles.coin_search}>
+            <input
+                className={styles.coin_input}
+                {...rest}
+            />
+        </div>
+    );
+};
+
+export default SearchBar;
